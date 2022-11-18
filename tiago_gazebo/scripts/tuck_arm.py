0f79edff0381efebe251dc5d0643c4e20b15e43c
sasilva1998 <sasilva1998@gmail.com>
2022-11-18 10:02:14 +0000

    fix: python version

--- tiago_gazebo/scripts/tuck_arm.py
+++ tiago_gazebo/scripts/tuck_arm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 import actionlib
@@ -6,20 +6,20 @@ from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
 from sensor_msgs.msg import JointState
 
 if __name__ == "__main__":
-  rospy.init_node("tuck_arm")
-  rospy.loginfo("Waiting for play_motion...")
-  client = actionlib.SimpleActionClient("play_motion", PlayMotionAction)
-  client.wait_for_server()
-  rospy.loginfo("...connected.")
+    rospy.init_node("tuck_arm")
+    rospy.loginfo("Waiting for play_motion...")
+    client = actionlib.SimpleActionClient("play_motion", PlayMotionAction)
+    client.wait_for_server()
+    rospy.loginfo("...connected.")
 
-  rospy.wait_for_message("joint_states", JointState)
-  rospy.sleep(3.0)
+    rospy.wait_for_message("joint_states", JointState)
+    rospy.sleep(3.0)
 
-  rospy.loginfo("Tuck arm...")
-  goal = PlayMotionGoal()
-  goal.motion_name = 'home'
-  goal.skip_planning = False
+    rospy.loginfo("Tuck arm...")
+    goal = PlayMotionGoal()
+    goal.motion_name = "home"
+    goal.skip_planning = False
 
-  client.send_goal(goal)
-  client.wait_for_result(rospy.Duration(10.0))
-  rospy.loginfo("Arm tucked.")
+    client.send_goal(goal)
+    client.wait_for_result(rospy.Duration(10.0))
+    rospy.loginfo("Arm tucked.")
